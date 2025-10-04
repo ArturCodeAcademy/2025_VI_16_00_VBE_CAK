1e50e9140771550d8698ddc5de4f1d17bfabab0f
ArturCodeAcademy <123232198+ArturCodeAcademy@users.noreply.github.com>
2025-10-04 16:04:35 +0200

    Add solutions

---
+++ Solutions/Math/Problem 0. 000.py
@@ -0,0 +1,3 @@
+a, b = map(int, input().split())
+print("Suma yra", a + b)
+print("Skirtumas yra", a - b)
---
+++ Solutions/Math/Problem 1. Lakštų plotas.py
@@ -0,0 +1,5 @@
+a1, a2 = map(int, input().split())
+s1 = a1 * a1
+s2 = a2 ** 2
+s_total = s1 + s2
+print("Bendras plotas", s_total, "kv. m")
